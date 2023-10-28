ed14e1a999aee49bb7cb8dc2b7ab1b60cbf858ca
Nico-242 <Nick9924@icloud.com>
2023-10-28 13:20:36 -0400

    HANDS WORK

--- file.py
+++ file.py
@@ -21,25 +21,51 @@ import sys
 # plt.subplot(144);plt.imshow(imgMerged[:,:,::-1]);plt.title("Merged Output");
 # plt.show()
 
-s = 0
-if(len(sys.argv) > 1) :
-    s = sys.argv[1]
+# s = 0
+# if(len(sys.argv) > 1) :
+#     s = sys.argv[1]
 
-source = cv2.VideoCapture(s)
+# source = cv2.VideoCapture(s)
 
-win_name = 'Camera Preview'
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+# win_name = 'Camera Preview'
+# cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-while cv2.waitKey(1) != 27:
-    has_frame,frame = source.read()
-    if not has_frame:
-        break
-    cv2.imshow(win_name, frame)
+# while cv2.waitKey(1) != 27:
+#     has_frame,frame = source.read()
+#     if not has_frame:
+#         break
+#     cv2.imshow(win_name, frame)
 
-source.release()
-cv2.waitKey()
-cv2.destroyAllWindow()
+# source.release()
+# cv2.waitKey(0)
+# cv2.destroyAllWindow()
 
-# cv2.waitKey() # This is necessary to be required so that the image doesn't close immediately.  
-# # It will run continuously until the key press.  
-# cv2.destroyAllWindows() 
+# # cv2.waitKey() # This is necessary to be required so that the image doesn't close immediately.  
+# # # It will run continuously until the key press.  
+# # cv2.destroyAllWindows() 
+
+
+
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, image = cap.read()
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks: # working with each hand
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 20 :
+                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+    cv2.imshow("Output", image)
+    cv2.waitKey(1)
